217b724cb3cab7caf2b7830a6b452a8aa2caea10
Andrei B <andreibarut@gmail.com>
2022-09-01 17:51:02 +0300

    latest exchange also in DOM, built skeleton for website, added scss

--- exchange-script.js
+++ exchange-script.js
@@ -1,10 +1,8 @@
 //^ INDEX FUNCTIONALITY - TODAY'S RATE
 
 //! here we will use the input from the user 
-let fromCurrency = "USD"; 
-let toCurrency = "RON";
-
-let requestURL = `https://api.exchangerate.host/convert?from=${fromCurrency}&to=${toCurrency}`;
+const currencyContainer = document.getElementById("currency-container");
+let requestURL = `https://api.exchangerate.host/latest?base=RON`;
 const request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
@@ -13,9 +11,14 @@ request.send();
 request.onload = function() {
   const response = request.response;
   console.log(response);
-  let exchangeRate = response.info.rate;
-  // line below will print to console the exchange rate from the selected currencies
-  console.log(exchangeRate);
+  console.log(response.rates);
+  for (const [key, value] of Object.entries(response.rates)) {
+    let p = document.createElement("p");
+    p.innerText = `${key} : ${value}`;
+    currencyContainer.appendChild(p);
+    
+  }
+
 }
 
 
